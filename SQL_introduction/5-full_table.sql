d8f418d08570664fe0e959dba56518fbbc293d99
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-08 06:51:56 +0100

    adds task 5.6

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- A script that that prints the full description of the table first_table
 -- from the database in the current database.
 -- The database name will be passed as argument of mysql command
-SHOW COLUMNS FROM first_table
+SHOW COLUMNS FROM hbtn_0c_0.first_table
