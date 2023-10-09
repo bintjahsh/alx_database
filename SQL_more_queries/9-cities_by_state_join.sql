d5736006089b6d6486463d6ed9a7b693f233e6b5
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-10 00:30:11 +0100

    adds task 9.1

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- A script lists all cities contained 
 -- in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
+SELECT * FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
