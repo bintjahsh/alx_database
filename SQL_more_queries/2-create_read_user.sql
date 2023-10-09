ea6c70a06c06b084164ad46a564c3f5303d12588
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 22:58:09 +0100

    adds task 2.1

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,10 +4,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user user_0d_2
 CREATE USER IF NOT EXISTS user_0d_2@localhost
-IDENTIFIED BY user_0d_2_pwd;
+IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant user_0d_2 SELECT privilege only
 GRANT SELECT
 ON hbtn_0d_2.*
 TO user_0d_2@localhost;
-
