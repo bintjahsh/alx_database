5a57c3509a365649f7e2a3f4eb19fe3d823eab90
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 08:12:11 +0100

    adds task 9.4

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,21 +1,21 @@
 -- A script that creates a table called second_table in the database hbtn_0c_0.
 -- The database name will be passed as argument of mysql command
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 )
 
 -- Insert data for 4 students into table second_table
-INSERT INTO hbtn_0c_0.second_table (
+INSERT INTO second_table (
     id,
     name,
     score
 )
 
 VALUES (
-    (1, 'John', 10)
-    (2, 'Alex', 3)
-    (3, 'Bob', 14)
-    (4, 'George', 8)
+    ('1', 'John', 10),
+    ('2', 'Alex', 3),
+    ('3', 'Bob', 14),
+    ('4', 'George', 8)
 )
