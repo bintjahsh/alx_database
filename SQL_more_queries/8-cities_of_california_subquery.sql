8c167c275838b24fd42bfe88901ca9b711a49a30
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 23:59:23 +0100

    adds task 8.1

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- A script lists all the cities of California that
 -- can be found in the database hbtn_0d_usa
-SELECT *
-FROM hbtn_0d_usa.*
-WHERE name = 'California'
+SELECT * FROM hbtn_0d_usa.*
+WHERE `name` = 'California'
 ORDER BY cities.id ASC;
