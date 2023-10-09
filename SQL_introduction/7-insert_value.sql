77a950678a842ef333ad3386c0abea7fcddd83b2
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 08:54:45 +0100

    adds task 9.7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,4 @@
 -- A script that inserts a new row in the table first_table
 -- in the database hbtn_0c_0 in the current database.
 -- The database name will be passed as argument of mysql command
-INSERT INTO first_table (id, name)
-VALUES ('89','Holberton School')
+INSERT INTO first_table (id, name) VALUES ('89','Holberton School')
--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,17 +1,14 @@
 -- A script that creates a table called second_table in the database hbtn_0c_0.
 -- The database name will be passed as argument of mysql command
-USE hbtn_0c_0
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
--- Insert data for 4 students into table second_table
-INSERT INTO second_table (id, name, score)
-VALUES (
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8)
-);
+-- Insert 4 records into table second_table
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10)
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3)
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14)
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8)
