7803bc025229d4495e385601747d7d7dfbc9e4f6
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 06:19:11 +0100

    adds task 6

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- A script that  lists all rows of the table first_table
+-- from the database in the current database. All fields will be included
+-- The database name will be passed as argument of mysql command
+SELECT * FROM first_table
