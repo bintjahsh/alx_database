641d1856e0d40332bec0b1bcaf5721229b76e454
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 23:38:50 +0100

    adds task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- A script  creates the database hbtn_0d_usa and the table
+-- cities in the database hbtn_0d_usa on this MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create the table cities in the database hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES states(id)
+)
