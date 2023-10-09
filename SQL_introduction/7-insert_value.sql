43eb243f23354449deaf24ceaf7172b95f4e7a84
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 06:46:11 +0100

    adds task 7.1 and 5.9

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,12 +1,5 @@
 -- A script that inserts a new row in the table first_table
 -- in the database hbtn_0c_0 in the current database.
 -- The database name will be passed as argument of mysql command
-USE hbtn_0c_0;
-INSERT INTO first_table(
-    `id`,
-    `name`,
-)
-VALUES (
-    '89',
-    'Holberton School'
-)
+INSERT INTO first_table (id, name)
+VALUES ('89','Holberton School')
