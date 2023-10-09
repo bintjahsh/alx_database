718840181de99c4c15e6a698424dc252d2b72ef1
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 22:48:47 +0100

    adds task 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+--  A script that creates the database hbtn_0d_2 and the user user_0d_2
+-- and grants them SELECT privilege only on this database in this server in localhost.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY user_0d_2_pwd;
+
+-- Grant user_0d_2 SELECT privilege only
+GRANT SELECT
+ON hbtn_0d_2.*
+TO user_0d_2@localhost;
+
