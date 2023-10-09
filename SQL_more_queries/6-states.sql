2589c20914cf66769470f3e688b727ddc9a673b4
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 23:30:44 +0100

    adds task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,9 +3,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Create table `states` in the database hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256),
-    UNIQUE(id)
+    UNIQUE(id),
     PRIMARY KEY (id)
 )
