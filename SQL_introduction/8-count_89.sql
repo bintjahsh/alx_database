ed9448892d458f18a83202088128437cb7ebf45f
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 06:56:57 +0100

    adds task 8.1 and 5.11

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,5 +1,6 @@
 -- A script that displays the number of records with id = 89
 --  in the table first_table in the database hbtn_0c_0.
 -- The database name will be passed as argument of mysql command
-SELECT * FROM first_table
+SELECT COUNT(id)
+FROM first_table
 WHERE id = 89
