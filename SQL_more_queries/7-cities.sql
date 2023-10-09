58949a4e4df527fc07d2f2d7518e27253425082b
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 23:42:38 +0100

    adds task 7.1

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     name VARCHAR(256) NOT NULL,
     UNIQUE (id),
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 )
