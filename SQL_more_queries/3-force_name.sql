66fa89c7a42183233985740aea8b9f2563776ded
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-09 23:04:41 +0100

    adds task 3

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- A script that creates the table force_name with two fields id and name
+-- in this server in localhost.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+)
